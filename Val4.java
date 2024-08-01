df0c15f934e43ef92fd767c6247be829ffef17a8
cedced1551 <ces0105@naver.com>
2024-08-01 15:18:07 +0900

    Create Val4.java

---
+++ Val4.java
@@ -0,0 +1,11 @@
+package valiable;
+
+public class Val4 {
+    public static void main(String[] args) {
+        int a; //하나씩 선언 가능
+        int b;
+
+        int c,d; //콤마(,)로 여러개 선언 가능
+
+    }
+}

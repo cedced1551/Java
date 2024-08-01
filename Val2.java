36f6e9fbf6f9be559da8a162b69f252cb9198a93
cedced1551 <ces0105@naver.com>
2024-08-01 15:17:10 +0900

    Create Val2.java

---
+++ Val2.java
@@ -0,0 +1,23 @@
+package valiable;
+
+public class Val2
+{
+    public static void main(String[] args) {
+        int a; //변수 선언 -> 숫자를 넣는 그릇
+        a = 20;//변수 초기화 -> 그릇에 담음
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+}
+
+/*
+int a
+숫자 정수를 보관할 수 있는 a라는 데이터 저장소 = 변수
+변수 a에는 정수를 보관 가능
+자바에서 = 기호는 오른쪽에 있는 값을 왼쪽에 저장한다는 뜻
+선언한 변수에 처음으로 값을 대입해서 저장하는 것을 변수 초기화
+
+ */

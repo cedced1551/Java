c2601be64842516c5ebc490e86bf643da02749a1
cedced1551 <ces0105@naver.com>
2024-08-01 15:16:51 +0900

    Create Val.java

---
+++ Val.java
@@ -0,0 +1,13 @@
+package valiable; //패키지이름 맞춰줘야함 (선언 위치가 같아야한다)
+
+public class Val1 {
+    public static void main(String[] args) { //psvm
+        System.out.println(20); //변경 10->20 sout
+        System.out.println(20); //변경 10->20
+        System.out.println(20); //변경 10->20
+        
+    }
+}
+//단순예제라면 직접 바꾸면 되지만
+//어딘가에 값을 보관해두고 값을 꺼내서 읽을 수 있는 저장소가 필요
+//데이터를 담을 그릇이 필요하다

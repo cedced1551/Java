6c307457dc9dd111df4d9de315cf34bf9c6b647b
cedced1551 <ces0105@naver.com>
2024-08-01 15:17:28 +0900

    Create Val3.java

---
+++ Val3.java
@@ -0,0 +1,13 @@
+package valiable;
+
+public class Val3 {
+    public static void main(String[] args) {
+        int a;//변수 선언
+        a = 10;//변수 초기화
+        System.out.println(a);
+        a = 50; //값 변경
+        System.out.println(a);
+
+    }
+}
+//변수의 값을 변경하면 변수에 있던 기존 값은 삭제된다.
